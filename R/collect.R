5d38d1b880836fe9215a6d0e4714b826859a334b
LiNk-NY <mramos09@gmail.com>
2019-05-01 12:32:33 -0400

    update collect.R function

--- R/collect.R
+++ R/collect.R
@@ -11,10 +11,23 @@
     newsfile
 }
 
-collect <- function(packageList) {
+collect <- function(packageList, outFile = tempfile()) {
     packageList <- setNames(packageList, packageList)
-    pkgs <- readLines(packageList)
-    unlist(lapply(pkgs, function(pkg) {
+    listNEWS <- lapply(packageList, function(pkg) {
         readLines(.findNEWSfile(pkg))
-    }))
+    })
+
+    indx <- lapply(listNEWS, function(pkgLines) {
+        vers <-
+            head(
+                grep("Changes in version", ignore.case = TRUE, pkgLines),
+            2L)
+        if (identical(length(vers), 1L))
+            length(pkgLines)
+        else
+            vers[[2L]] - 1L
+    })
+    newslines <- mapply(function(x, y) x[seq_len(y)], x = listNEWS, y = indx)
+    writeLines(text = unlist(newslines), con = file(outFile))
 }
+
