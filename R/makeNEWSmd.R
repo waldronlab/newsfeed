a59b96acb52b29ca222a8b1736aeb489120609d5
LiNk-NY <marcel.ramosperez@roswellpark.org>
2019-10-21 11:14:27 -0400

    include all files

---
+++ R/makeNEWSmd.R
@@ -0,0 +1,36 @@
+.addPkg2NEWS <- function(pkg, nfile) {
+    newslines <- readLines(nfile)
+    if (missing(pkg))
+        pkg <- devtools::as.package(".")$package
+    pkginnews <- grepl(pkg, newslines[1])
+    if (pkginnews)
+        stop("The NEWS file should not contain the package name;",
+        "\n Test package with 'utils::news'")
+    c(pkg, paste0(rep("-", 64), collapse = ""), "", newslines)
+}
+
+#' Create a NEWS.md file from NEWS file
+#'
+#' This function will add a package name line to the
+#' current NEWS file and write a NEWS.md file for
+#' viewing on GitHub. Future iterations will simplify
+#' formatting changes to the NEWS file so that it can
+#' be converted to a markdown file. Current files
+#' should have markdown syntax in the NEWS file.
+#'
+#' @param newsfile character(1) The location of the NEWS file usually in
+#'     'inst/NEWS' (default)
+#' @param overwite logical(1) Whether to overwrite the existing NEWS.md file
+#'     (default: FALSE)
+#'
+#' @return Saves a file with the name "NEWS.md" in the package directory
+#'
+#' @export
+makeNEWSmd <- function(newsfile = "inst/NEWS", overwrite = FALSE) {
+    if (file.exists("NEWS.md") && !overwrite) {
+        stop("NEWS.md file exists")
+    } else {
+        newsout <- .addPkg2NEWS(newsfile)
+        writeLines(newsout, "NEWS.md")
+    }
+}
